3ecf689f993229c7adba225af6aff97fe8364439
Leon Berni <leon.berni@valid.com>
2026-07-21 13:53:43 -0300

    Updated URL

--- IDDigitalSDK-SPM/Package.swift
+++ IDDigitalSDK-SPM/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDDigitalSDK",
-            url: "https://your-storage.com/IDDigitalSDK.zip",
+            url: "https://github.com/leon-valid/valid-spm/releases/tag/1.0.0",
             checksum: "43d3d030dc310184fcbdc017e9b6abded1f830d0acf6752b25e1c35c405620d1"
         )
     ]
